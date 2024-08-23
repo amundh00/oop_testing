03a4036b81aa541bfe7831ce45a020e5a519ba40
Amund Halgunset <amundhalgunset@gmail.com>
2024-08-23 12:10:06 +0200

    yessir

--- scriptTwo.js
+++ scriptTwo.js
@@ -1,15 +1,16 @@
-async function populate() {
+async function populate(){
     const api =
     "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
-
     const response = await fetch(api);
     const superHeroesText = await response.text();
 
     const superHeroes = JSON.parse(superHeroesText);
     console.log(superHeroes);
     populateHeader(superHeroes);
-    populateHeroes(superHeroes);
+
     
+    const heroes = makeHeroes(superHeroes.members)
+    populateHeroes(heroes);
 }
 
 populate();
@@ -20,44 +21,19 @@ function populateHeader(obj) {
     myH1.textContent = obj.squadName;
     header.appendChild(myH1);
 
-    const myPara = document.createElement("P");
-    myPara.textContent = `Hometown: ${obj.homeTown} // Formed: ${obj.formed}`;
+    const myPara = document.createElement("p");
+    myPara.textContent = `Hometown: ${obj.homeTown} // form: ${obj.formed}`;
     header.appendChild(myPara);
 }
 
-function populateHeroes(obj) {
-    const section = document.querySelector("section");
-    const heroes = obj.members;
+function populateHeroes(arr){
+   // const heroes = obj.members
 
-    for (const hero of heroes) {
-        const myArticle = document.createElement("article");
-        const myH2 = document.createElement("h2");
-        const myPara1 = document.createElement("p");
-        const myPara2 = document.createElement("p");
-        const myPara3 = document.createElement("p");
-        const myList = document.createElement("ul");
-
-        myH2.textContent = hero.name;
-        myPara1.textContent = `Secret indentity: ${hero.secretIdentity}`;
-        myPara2.textContent = `Age: ${hero.age}`;
-        myPara3.textContent = `Superpowers:`;
-
-        const superPowers = hero.powers;
-        for (const power of superPowers) {
-            const listItem = document.createElement("li");
-            listItem.textContent = power;
-            myList.appendChild(listItem);
-        }
-        myArticle.appendChild(myH2);
-        myArticle.appendChild(myPara1);
-        myArticle.appendChild(myPara2);
-        myArticle.appendChild(myPara3);
-        myArticle.appendChild(myList);
-
-        section.appendChild(myArticle);
-
+    for (const hero of arr){
+        hero.makeArticle();
     }
 }
+
 
 class Superhero {
     name;
@@ -65,13 +41,51 @@ class Superhero {
     secretIdentity;
     powers;
 
-    constructor (name, age, secretIdentity, powers) {
+    constructor(name, age, secretIdentity, powers){
         this.name = name;
         this.age = age;
         this.secretIdentity = secretIdentity;
         this.powers = powers;
     }
-    makeArticle () {
-        
-    }
+
+    makeArticle (){
+        const section = document.querySelector("section")
+
+        const myArticle = document.createElement("article");
+const myH2 = document.createElement("h2");
+const myPara1 = document.createElement("p");
+const myPara2 = document.createElement("p");
+const myPara3 = document.createElement("p");
+const myList = document.createElement("ul");
+
+myH2.textContent = this.name;
+myPara1.textContent = `Secret identify: ${this.secretIdentity}`;
+myPara2.textContent = `Age: ${this.age}`;
+myPara3.textContent = `Superpowers:`;
+
+const superPowers = this.powers;
+for(const power of superPowers){
+    const listItem = document.createElement("li");
+    listItem.textContent = power;
+    myList.appendChild(listItem);
 }
+
+myArticle.appendChild(myH2);
+myArticle.appendChild(myPara1);
+myArticle.appendChild(myPara2);
+myArticle.appendChild(myPara3);
+myArticle.appendChild(myList);
+
+section.appendChild(myArticle);
+}
+    }
+
+    function makeHeroes (arr){
+        console.log(arr);
+        let myHeroes = [];
+        for (let hero of arr){
+            myHeroes.push(new Superhero(hero.name, hero.age, hero.secretIdentity, hero.powers));
+
+        }
+        return myHeroes;
+    }
